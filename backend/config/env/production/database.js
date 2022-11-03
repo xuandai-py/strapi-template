2c29f47c077565c5a1707df819e397f7fd8ceab7
hermitcrab <hermitcrab@pop-os.localdomain>
2022-11-04 01:28:49 +0700

    modify db connection

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -4,14 +4,14 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: {
-        ca: env('DATABASE_CA')
-      },
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'xd'),
+      user: env('DATABASE_USERNAME', 'xd'),
+      password: env('DATABASE_PASSWORD', 'xd'),
+      // ssl: {
+      //   ca: env('DATABASE_CA')
+      // },
     },
     debug: false,
   },
